c4e6a20f06988a25c35faaf3cc4e921272c8abcf
Eric-Paul Lecluse <e@epologee.com>
2016-03-29 13:35:06 +0200

    Simplify template to support only Swift projects

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -70,15 +70,7 @@ module Pod
     def run
       @message_bank.welcome_message
 
-      framework = self.ask_with_answers("What language do you want to use?", ["ObjC", "Swift"]).to_sym
-      case framework
-        when :swift
-          ConfigureSwift.perform(configurator: self)
-
-        when :objc
-          ConfigureIOS.perform(configurator: self)
-      end
-
+      ConfigureSwift.perform(configurator: self)
 
       replace_variables_in_files
       clean_template_files
